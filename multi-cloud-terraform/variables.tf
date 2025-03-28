58f7fca088acebd45c3b459c4056265e7e232242
Adishri <150842680+adijain-99@users.noreply.github.com>
2025-03-28 00:21:21 +0000

    Update variables.tf

--- multi-cloud-terraform/variables.tf
+++ multi-cloud-terraform/variables.tf
@@ -1,1 +1,13 @@
+cat << EOF > variables.tf
+variable "container_count" {
+  description = "Number of container instances"
+  type        = number
+  default     = 3
+}
 
+variable "container_image" {
+  description = "Docker image to use"
+  type        = string
+  default     = "nginx:latest"
+}
+EOF

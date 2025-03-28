e874c1a137ad0e58b6f3782b0c0e4a1b802ffe0d
Adishri <150842680+adijain-99@users.noreply.github.com>
2025-03-28 00:15:05 +0000

    Update main.tf

--- multi-cloud-terraform/main.tf
+++ multi-cloud-terraform/main.tf
@@ -1,1 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
 
+provider "docker" {}
+
+resource "docker_network" "web_network" {
+  name = "web-app-network"
+}
+
+resource "docker_container" "web_app" {
+  count = var.container_count
+  image = var.container_image
+  name  = "web-app-${count.index + 1}"
+  
+  networks_advanced {
+    name = docker_network.web_network.name
+  }
+
+  ports {
+    internal = 80
+    external = 8080 + count.index
+  }
+}

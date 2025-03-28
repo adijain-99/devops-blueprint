0c4d6ed2bd3030b3b00ad9a0dc01d25e003250e6
Adishri <150842680+adijain-99@users.noreply.github.com>
2025-03-28 00:22:07 +0000

    Update outputs.tf

--- multi-cloud-terraform/outputs.tf
+++ multi-cloud-terraform/outputs.tf
@@ -1,1 +1,16 @@
+cat << EOF > outputs.tf
+output "container_ids" {
+  description = "IDs of the created containers"
+  value       = docker_container.web_app[*].id
+}
 
+output "container_names" {
+  description = "Names of the created containers"
+  value       = docker_container.web_app[*].name
+}
+
+output "network_name" {
+  description = "Name of the created docker network"
+  value       = docker_network.web_network.name
+}
+EOF
